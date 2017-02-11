d146278c9cc476563a7c10290d0fe1913adbffb5
kovacsmate97 <kovimatyi8@freemail.hu>
2017-02-11 16:46:29 +0100

    Hozááadva 4 alapművelet

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double Osszead(double egyikszam, double masikszam)
+        {
+            return egyikszam + masikszam;
+        }
+        public static double Kivon(double egyikszam, double masikszam)
+        {
+            return egyikszam - masikszam;
+        }
+        public static double Szorzas(double egyikszam, double masikszam)
+        {
+            return egyikszam * masikszam;
+        }
+        public static double Osztas(double egyikszam, double masikszam)
+        {
+            return egyikszam / masikszam;
+        }
+    }
+}
